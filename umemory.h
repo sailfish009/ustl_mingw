44ee433576e013fa1de6ea3e7c1a79aa5b914978
Mike Sharov <msharov@users.sourceforge.net>
2018-08-02 19:44:53 -0400

    Do not use exchange in pre-14 code. Fixes #87

--- umemory.h
+++ umemory.h
@@ -173,7 +173,7 @@ private:
 public:
     inline constexpr		shared_ptr (void)		: _p (nullptr) {}
     inline explicit		shared_ptr (pointer p)		: _p (new container (p)) {}
-    inline			shared_ptr (shared_ptr&& p)	: _p (exchange (p._p, nullptr)) {}
+    inline			shared_ptr (shared_ptr&& p)	: _p (p._p) { p._p = nullptr; }
     inline			shared_ptr (const shared_ptr& p): _p (p._p) { if (_p) ++_p->refs; }
     inline			~shared_ptr (void)		{ reset(); }
     inline constexpr size_t	use_count (void) const		{ return _p ? _p->refs : 0; }
